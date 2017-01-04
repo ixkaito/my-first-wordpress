73ac39d761941b7f0c5a4206ef042ab66ff9fbce
Kite <ixkaito@gmail.com>
2017-01-05 04:08:40 +0900

    Improve footer.php

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <footer class="page-footer">
 	<div class="banner-area">
 		<ul class="banner-list">
-			<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-			<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-			<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+			<li><a href="#"><img src="http://placehold.jp/320x80.png"></a></li>
+			<li><a href="#"><img src="http://placehold.jp/320x80.png"></a></li>
+			<li><a href="#"><img src="http://placehold.jp/320x80.png"></a></li>
 		</ul>
 		<div class="back-to-top">
-			<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
+			<a href="#"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/images/arrow-up.png" srcset="<?php echo esc_url( get_theme_file_uri() ); ?>/images/arrow-up@2x.png 2x" alt="">TOP</a>
 		</div>
 	</div>
 	<div class="copyright">
-		<p>Copyright ©  Gijutsu-Hyohron Co., Ltd.</p>
+		<p>Copyright © Gijutsu-Hyohron Co., Ltd.</p>
 	</div>
 </footer>
 
