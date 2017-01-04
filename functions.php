a346eeffdb947a2638238c3c22be8cfa41a97b76
Kite <ixkaito@gmail.com>
2017-01-05 01:52:27 +0900

    Add firstwp_setup

---
+++ functions.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * My First WordPress Theme functions and definitions
+ */
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function firstwp_setup() {
+	/*
+	 * Make theme available for translation.
+	 * If you're building a theme based on Twenty Seventeen, use a find and replace
+	 * to change 'firstwp' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'firstwp' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	add_image_size( 'firstwp-featured-image', 2000, 1200, true );
+
+	add_image_size( 'firstwp-thumbnail-avatar', 100, 100, true );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'top'    => __( 'Top Menu', 'firstwp' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+		'gallery',
+		'audio',
+	) );
+
+	// Add theme support for Custom Logo.
+	add_theme_support( 'custom-logo', array(
+		'width'       => 250,
+		'height'      => 250,
+		'flex-width'  => true,
+	) );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	/*
+	 * This theme styles the visual editor to resemble the theme style,
+	 * specifically font, colors, and column width.
+	 */
+	add_editor_style( array( 'assets/css/editor-style.css', firstwp_fonts_url() ) );
+}
+// add_action( 'after_setup_theme', 'firstwp_setup' );
