3a8123b6040b2f8e392d93cd7af4776320e2cfb6
Kite <ixkaito@gmail.com>
2017-01-05 03:55:50 +0900

    Move footer to footer.php

--- index.php
+++ index.php
@@ -185,20 +185,5 @@
 		</aside>
 	</div>
 </div>
-<footer class="page-footer">
-	<div class="banner-area">
-		<ul class="banner-list">
-			<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-			<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-			<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-		</ul>
-		<div class="back-to-top">
-			<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
-		</div>
-	</div>
-	<div class="copyright">
-		<p>Copyright ©  Gijutsu-Hyohron Co., Ltd.</p>
-	</div>
-</footer>
 
 <?php get_footer();
