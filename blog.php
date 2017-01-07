1d076e476ecff9714e0781edd5db9240d04c417b
Kite <ixkaito@gmail.com>
2017-01-08 06:15:48 +0900

    Rename index.php to blog.php and front-page.php to index.php

---
+++ blog.php
@@ -0,0 +1,189 @@
+<?php get_header(); ?>
+
+<div class="page-title">
+	<h1>ブログ</h1>
+</div>
+<div class="content-area has-side-col">
+	<div class="main-column">
+		<div class="content-box">
+			<h2 class="box-heading">blog</h2>
+			<div class="box-content">
+			<ul class="archive">
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+				<li class="item-archive">
+					<div class="time-and-thumb-archive">
+						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+						<p class="thumb thumb-archive"><img src="http://placehold.it/190x130"></p>
+					</div>
+					<div class="data-archive">
+						<p class="list-categories-archive">カテゴリ名</p>
+						<h2 class="title-archive">記事タイトル記事タイトル記事タイトル記事タイトル</h2>
+						<p class="list-tags-archive">タグ, タグ, タグ, タグ</p>
+					</div>
+				</li>
+			</ul>
+			</div>
+		</div>
+		<div class="wp-pagenavi">
+			<a href="#" class="previouspostslink" rel="prev "><img class="arrow" src="images/arrow-left.png" srcset="images/arrow-left@2x.png 2x" alt=""></a>
+			<a class="page larger" href="#">1</a>
+			<a class="page larger" href="#">2</a>
+			<a class="page larger" href="#">3</a>
+			<a class="page larger" href="#">4</a>
+			<a class="page larger" href="#">5</a>
+			<span class="extend">...</span>
+			<a class="page larger" href="#">10</a>
+			<a class="page larger" href="#">20</a>
+			<a class="page larger" href="#">30</a>
+			<a href="#" class="nextpostlink" rel="next "><img class="arrow" src="images/arrow-right.png" srcset="images/arrow-right@2x.png 2x" alt=""></a>
+		</div>
+	</div>
+
+	<div class="side-column">
+		<nav>
+			<div class="side-list category-nav">
+				<div class="sidebar-widget">
+					<h2 class="box-heading box-heading-side-bar">カテゴリ一覧</h2>
+					<div class="box-content">
+						<ul>
+							<li class="cat-item"><a href="#">カテゴリ1</a></li>
+							<li class="cat-item"><a href="#">カテゴリ2</a></li>
+							<li class="cat-item"><a href="#">カテゴリ3</a></li>
+							<li class="cat-item"><a href="#">カテゴリ4</a></li>
+							<li class="cat-item"><a href="#">カテゴリ5</a></li>
+						</ul>
+					</div>
+				</div>
+			<div class="side-list monthly-nav">
+				<div class="sidebar-widget">
+					<h2 class="box-heading box-heading-side-bar">月別アーカイブ</h2>
+					<div class="box-content">
+						<ul>
+							<li><a href="#">2016年12月</a></li>
+							<li><a href="#">2016年11月</a></li>
+							<li><a href="#">2016年10月</a></li>
+							<li><a href="#">2016年9月</a></li>
+							<li><a href="#">2016年8月</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<aside>
+			<div class="social-widgets">
+				<div class="sidebar-widget">
+					<h2 class="box-heading box-heading-side-bar">Facebook</h2>
+					<div class="box-content">
+						widget
+					</div>
+				</div>
+				<div class="sidebar-widget">
+					<h2 class="box-heading box-heading-side-bar">Twitter</h2>
+					<div class="box-content">
+						widget
+					</div>
+				</div>
+			</div>
+		</aside>
+	</div>
+</div>
+
+<?php get_footer();
