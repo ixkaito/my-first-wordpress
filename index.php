33737d48300e0703e51bee5a968eb72657b0e69d
Kite <ixkaito@gmail.com>
2017-01-13 02:46:59 +0900

    Add sidebar.php

--- index.php
+++ index.php
@@ -55,53 +55,8 @@
 
 	</div>
 
-	<div class="side-column">
-		<nav>
-			<div class="side-list category-nav">
-				<div class="sidebar-widget">
-					<h2 class="box-heading box-heading-side-bar">カテゴリ一覧</h2>
-					<div class="box-content">
-						<ul>
-							<li class="cat-item"><a href="#">カテゴリ1</a></li>
-							<li class="cat-item"><a href="#">カテゴリ2</a></li>
-							<li class="cat-item"><a href="#">カテゴリ3</a></li>
-							<li class="cat-item"><a href="#">カテゴリ4</a></li>
-							<li class="cat-item"><a href="#">カテゴリ5</a></li>
-						</ul>
-					</div>
-				</div>
-			<div class="side-list monthly-nav">
-				<div class="sidebar-widget">
-					<h2 class="box-heading box-heading-side-bar">月別アーカイブ</h2>
-					<div class="box-content">
-						<ul>
-							<li><a href="#">2016年12月</a></li>
-							<li><a href="#">2016年11月</a></li>
-							<li><a href="#">2016年10月</a></li>
-							<li><a href="#">2016年9月</a></li>
-							<li><a href="#">2016年8月</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</nav>
-		<aside>
-			<div class="social-widgets">
-				<div class="sidebar-widget">
-					<h2 class="box-heading box-heading-side-bar">Facebook</h2>
-					<div class="box-content">
-						widget
-					</div>
-				</div>
-				<div class="sidebar-widget">
-					<h2 class="box-heading box-heading-side-bar">Twitter</h2>
-					<div class="box-content">
-						widget
-					</div>
-				</div>
-			</div>
-		</aside>
-	</div>
+	<?php get_sidebar(); ?>
+
 </div>
 
 <?php get_footer();
