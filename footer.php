a292ed90d0c19dd4552e7837c6f862a8508e049b
Kite <ixkaito@gmail.com>
2017-01-13 06:44:08 +0900

    Improve the style of footer widgets

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <footer class="page-footer">
-	<div class="banner-area">
+	<div class="footer-widget-area">
 
-			<ul class="banner-list">
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-			</ul>
+		<?php if ( is_active_sidebar( 'footer' ) ) : ?>
+			<div class="footer-widgets">
+				<?php dynamic_sidebar( 'footer' ); ?>
+			</div>
+		<?php endif; ?>
 
 		<div class="back-to-top">
 			<a href="#"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/images/arrow-up.png" srcset="<?php echo esc_url( get_theme_file_uri() ); ?>/images/arrow-up@2x.png 2x" alt="">TOP</a>
