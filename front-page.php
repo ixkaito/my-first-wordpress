9e6bca541092fe8a582bfd07ace34094dac8574f
Kite <ixkaito@gmail.com>
2017-01-14 18:54:48 +0900

    Test for front page panel

--- front-page.php
+++ front-page.php
@@ -5,11 +5,25 @@
 <div class="content-area has-side-col">
 	<div class="main-column">
 
-		<?php for ( $i = 1; $i <= 5; $i++ ) : ?>
+		<?php for ( $i = 1; $i <= 5; $i++ ) :
 
-			<?php echo $i; ?>
+			if ( is_a( $partial, 'WP_Customize_Partial' ) ) {
+				$id = str_replace( 'panel_', '', $partial->id );
+			}
 
-		<?php endfor; ?>
+			$post = get_post( get_theme_mod( 'panel_' . $id ) );
+			setup_postdata( $post );
+			set_query_var( 'panel', $id ); ?>
+
+			<h2 class="box-heading"><?php the_title(); ?></h2>
+
+			<div class="box-content">
+				<?php the_content(); ?>
+			</div>
+
+			<?php wp_reset_postdata();
+
+		endfor; ?>
 
 	</div>
 
