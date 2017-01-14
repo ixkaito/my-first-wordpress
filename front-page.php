639c335a9860714c8242114c8a324f27718aa6f1
Kite <ixkaito@gmail.com>
2017-01-14 18:42:09 +0900

    Test for loop

--- front-page.php
+++ front-page.php
@@ -4,69 +4,13 @@
 </div>
 <div class="content-area has-side-col">
 	<div class="main-column">
-		<h1 class="box-heading box-heading-main-col">Blog</h1>
-		<div class="box-content">
-			<ul class="archive">
-				<li class="item-archive">
-					<div class="time-and-thumb-archive">
-						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
-						<p class="thumb thumb-archive"><a href="single.html"><img src="http://placehold.it/190x130"></a></p>
-					</div>
-					<div class="data-archive">
-						<p class="list-categories-archive"><a href="archive.html">カテゴリ名</a></p>
-						<h2 class="title-archive"><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></h2>
-						<p class="list-tags-archive"><a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a></p>
-					</div>
-				</li>
-				<li class="item-archive">
-					<div class="time-and-thumb-archive">
-						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
-						<p class="thumb thumb-archive"><a href="single.html"><img src="http://placehold.it/190x130"></a></p>
-					</div>
-					<div class="data-archive">
-						<p class="list-categories-archive"><a href="archive.html">カテゴリ名</a></p>
-						<h2 class="title-archive"><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></h2>
-						<p class="list-tags-archive"><a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a></p>
-					</div>
-				</li>
-				<li class="item-archive">
-					<div class="time-and-thumb-archive">
-						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
-						<p class="thumb thumb-archive"><a href="single.html"><img src="http://placehold.it/190x130"></a></p>
-					</div>
-					<div class="data-archive">
-						<p class="list-categories-archive"><a href="archive.html">カテゴリ名</a></p>
-						<h2 class="title-archive"><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></h2>
-						<p class="list-tags-archive"><a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a></p>
-					</div>
-				</li>
-				<li class="item-archive">
-					<div class="time-and-thumb-archive">
-						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
-						<p class="thumb thumb-archive"><a href="single.html"><img src="http://placehold.it/190x130"></a></p>
-					</div>
-					<div class="data-archive">
-						<p class="list-categories-archive"><a href="archive.html">カテゴリ名</a></p>
-						<h2 class="title-archive"><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></h2>
-						<p class="list-tags-archive"><a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a></p>
-					</div>
-				</li>
-				<li class="item-archive">
-					<div class="time-and-thumb-archive">
-						<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
-						<p class="thumb thumb-archive"><a href="single.html"><img src="http://placehold.it/190x130"></a></p>
-					</div>
-					<div class="data-archive">
-						<p class="list-categories-archive"><a href="archive.html">カテゴリ名</a></p>
-						<h2 class="title-archive"><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></h2>
-						<p class="list-tags-archive"><a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a></p>
-					</div>
-				</li>
-			</ul>
-			<div class="nav-to-archives">
-				<a href="#">過去の記事を読む <img class="arrow" src="images/arrow-right.png" srcset="images/arrow-right@2x.png 2x" alt=""></a>
-			</div>
-		</div>
+
+		<?php for ( $i = 1; $i <= 5; $i++ ) : ?>
+
+			<?php echo $i; ?>
+
+		<?php endfor; ?>
+
 	</div>
 
 	<?php get_sidebar(); ?>
