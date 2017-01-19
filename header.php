abafb1f2e1cb3396efe335be333dd924c4787a74
Kite <ixkaito@gmail.com>
2017-01-19 10:29:22 +0900

    Improve the layout of the header

--- header.php
+++ header.php
@@ -9,26 +9,22 @@
 
 <body <?php body_class(); ?>>
 
-	<header class="page-header">
+	<header class="page-header<?php echo has_nav_menu( 'global' ) ? ' has-nav-menu' : ''; ?>">
 
-		<div class="header-area">
-
-			<div class="panel-site-title">
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></p>
-				<p class="site-subtitle"><?php bloginfo( 'description' ); ?></p>
-			</div>
-
-			<?php if ( has_nav_menu( 'global' ) ) : ?>
-
-				<?php wp_nav_menu( array(
-					'theme_location'  => 'global',
-					'container'       => 'nav',
-					'container_class' => 'global-nav',
-					'menu_id'         => 'global-menu',
-				) ); ?>
-
-			<?php endif; ?>
-
+		<div class="panel-site-title">
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+			<p class="site-subtitle"><?php bloginfo( 'description' ); ?></p>
 		</div>
+
+		<?php if ( has_nav_menu( 'global' ) ) : ?>
+
+			<?php wp_nav_menu( array(
+				'theme_location'  => 'global',
+				'container'       => 'nav',
+				'container_class' => 'global-nav',
+				'menu_id'         => 'global-menu',
+			) ); ?>
+
+		<?php endif; ?>
 
 	</header>
